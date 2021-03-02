a502d88c528eaf34a283f13d6c7a13e66fe68291
wsherys <wshery@gmail.com>
2021-03-02 23:21:44 +0700

    upd

--- application/controllers/PageArtikel.php
+++ application/controllers/PageArtikel.php
@@ -14,6 +14,23 @@ class PageArtikel extends CI_Controller {
 	  
 		//mengubah data json menjadi data array asosiatif
 		$result=json_decode($content,true);
+
+		$data=array('result'=>$result);
+
+		$this->load->view('list_artikel',$data);
+
+	}
+
+	public function page_view($id){
+
+		//ambil data json dari file
+		$content=file_get_contents("http://127.0.0.1:8000/api/artikel/$id");
+
+		//mengubah standar encoding
+		$content=utf8_encode($content);
+	  
+		//mengubah data json menjadi data array asosiatif
+		$result=json_decode($content,true);
 		
 		//looping data menggunakan foreach
 		// foreach ($result as $value) {
@@ -23,14 +40,16 @@ class PageArtikel extends CI_Controller {
 		//  echo "TGL UPDATE : ".$value['updated_at']."<br>";
 		// }
 
-		$data=array('result'=>$result);
-
-		$this->load->view('list_artikel',$data);
+		$data=array('id'=>$id,
+		'author'=>$value['author'],
+		'text'=>$value['text'],
+		);
+		// var_dump($data);
+		// $this->load->view('page_view',$data);
+	}
+	public function page_edit($id){
 
 	}
-
-	public function page_view($id){}
-	public function page_edit($id){}
 	public function update(){}
 	public function delete(){}
 
